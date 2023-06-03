18c2b532fcaf8e28fadae8aa04d2b900ef85f539
chris <chris@DESKTOP-AE7282J>
2023-06-03 17:35:04 -0600

    Habran menos commits dado que tuve ciertos problemas ya que cambie el M.2 de mi computadora y tuve que re-instalar todo de nuevo asi como mis proyectos pasarlos de nuevo En realidad este es el quinto commit Pagina al 95% con estilos y estructura

---
+++ src/main/java/com/tiendaPractica1/tiendaPractica1/TiendaPractica1Application.java
@@ -0,0 +1,13 @@
+package com.tiendaPractica1.tiendaPractica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaPractica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaPractica1Application.class, args);
+	}
+
+}
